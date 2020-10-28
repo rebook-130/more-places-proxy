d59f44e103878f877785a2fab9f720c1e2eb7937
James Scolamieri <james.scolamieri@gmail.com>
2020-10-28 12:01:46 -0700

    updated to port 80

--- proxy/server.js
+++ proxy/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 80;
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use('/rooms/:roomId', express.static(path.join(__dirname, 'public')));
