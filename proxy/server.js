5b6b7d621f97a4fada3a2a985f85a8fca527196e
James Scolamieri <james.scolamieri@gmail.com>
2020-10-28 22:13:08 -0700

    changed proxy details

--- proxy/server.js
+++ proxy/server.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 3000;
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use('/rooms/:roomId', express.static(path.join(__dirname, 'public')));
 
 // Photo-gallery
-app.use('/api/photogallery/:roomId', createProxyMiddleware({ target: `http://54.219.99.93:3001/`, changeOrigin: true}));
+// app.use('/api/photogallery/:roomId', createProxyMiddleware({ target: `http://54.219.99.93:3001/`, changeOrigin: true}));
+app.use('/api/rooms/:roomId/photos', createProxyMiddleware({ target: 'http://18.144.16.144:3001/', changeOrigin: true, })
+);
 
 // Calendar
-app.use('/api/calendar', createProxyMiddleware({ target: `http://3.138.89.74:3002/`, changeOrigin: true}));
+app.use('/api/calendar', createProxyMiddleware({ target: `http://3.138.89.74:3002/`, changeOrigin: true }));
 
 // Reviews
-app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://3.136.76.170:3003/`, changeOrigin: true}));
+app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://3.136.76.170:3003/`, changeOrigin: true }));
 
 // More places
-const url = 'http://3.138.195.85:3004/'
-app.use('/api/**', createProxyMiddleware({ target: url, changeOrigin: true}));
+
+app.use('/api/users/**', createProxyMiddleware({ target: 'http://3.138.195.85:3004/', changeOrigin: true}));
 
 
 app.listen(port, () => {
